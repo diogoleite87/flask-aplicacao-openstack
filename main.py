640ea10a1e13180c043f293ba008c082882b9cb9
Diogo Leite <diogoleite87@gmail.com>
2023-11-09 11:10:14 -0300

    vamos por favor

--- main.py
+++ main.py
@@ -1,37 +1,53 @@
 # -*- coding: utf-8 -*-
 from flask import Flask, request
+import logging
 
 app = Flask(__name__)
 
 usuarios = []
 
+# Configuração de logging
+logging.basicConfig(level=logging.INFO,
+                    format='%(asctime)s - %(levelname)s - %(message)s')
+
 
 @app.route('/')
 def hello_world():
-    return """
+    # Obtém o endereço IP do usuário
+    user_ip = request.remote_addr
+
+    # Verifica se o IP do usuário já está na lista de usuários
+    if user_ip not in usuarios:
+        usuarios.append(user_ip)
+        # Registra a conexão do usuário no log
+        logging.info(f'Novo usuário conectado: {user_ip}')
+
+    # Retorna a página HTML
+    return f"""
     <!DOCTYPE html>
     <html>
     <head>
         <title>Minha Página Flask</title>
         <style>
-            body {
+            body {{
                 font-family: Arial, sans-serif;
                 background-color: #f0f0f0;
                 text-align: center;
                 margin: 100px;
-            }
-            h1 {
+            }}
+            h1 {{
                 color: #333;
-            }
-            p {
+            }}
+            p {{
                 font-size: 18px;
                 color: #555;
-            }
+            }}
         </style>
     </head>
     <body>
         <h1>Olá, Mundo!</h1>
-        <p>Esta é uma página bonita em Flask.</p>
+        <p>Usuários conectados: {len(usuarios)}</p>
+        <p>IPs dos usuários: {', '.join(usuarios)}</p>
     </body>
     </html>
     """
