bd4110f5b96c3d8a1e67d2885365191018b89b62
Diogo Leite <diogoleite87@gmail.com>
2023-11-09 03:08:35 -0300

    fix: interface port

--- main.py
+++ main.py
@@ -1,12 +1,22 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
+
+# Lista para rastrear os IPs dos usuários
+usuarios = []
 
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    # Obtém o IP do usuário atual
+    ip = request.remote_addr
+
+    # Adiciona o IP à lista se ainda não estiver lá
+    if ip not in usuarios:
+        usuarios.append(ip)
+
+    return f'IP da Máquina: {request.host}, Porta: {request.environ["SERVER_PORT"]}, Total de Usuários: {len(usuarios)}'
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0', port=5000)
